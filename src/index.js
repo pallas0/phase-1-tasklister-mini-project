0b5ac6f67487792d93865f1d9a63f6db427a3dd8
amelia.risner0@gmail.com <arisner@berkeley.edu>
2022-01-18 10:14:49 -0800

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo(e.target["new-task-description"].value);
+  })
+
+  function handleToDo(todo) {
+    let p = document.createElement('p');
+    let btn = document.createElement("button");
+    btn.textContent = 'x';
+    p.textContent = `${todo} `;
+    p.appendChild(btn);
+    console.log(p)
+    document.querySelector('#list').append(p)
+
+    btn.addEventListener("click", (e) => {
+      p.remove();
+    })
+  }
 });
